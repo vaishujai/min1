90f9ec0e2f3cba419ca57f82beea933c1ee80d34
Vaishnavi <vj222@njit.edu>
2018-10-04 13:48:26 -0400

    Feature: Using a factory(In progress)

--- public/index.php
+++ public/index.php
@@ -1,8 +1,68 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: vaishnavi
- * Date: 10/2/18
- * Time: 7:31 PM
+
  */
-echo 'Hello World';
+main::start();
+class main {
+
+    static public function start() {
+        $records = csv::getRecords();
+        $table = html::generateTAble($records);
+
+        system::printPage($table);
+
+    }
+
+}
+
+class csv {
+    static public function getRecords() {
+        $make = 'ford';
+        $model = 'taurus';
+        $car = AutomobileFactory::create($make,$model);
+
+        $records[] = $car
+
+        print_r($records);
+        return $records;
+    }
+}
+
+class html {
+    static public function generateTable($records) {
+        $table =  $records;
+       return  $table;
+
+
+    }
+}
+
+class system {
+    static public function printPage($page) {
+        echo $page;
+    }
+}
+class Automobile
+{
+    private $vehicleMake;
+    private $vehicleModel;
+
+    public function __construct($make, $model)
+    {
+        $this->vehicleMake = $make;
+        $this->vehicleModel = $model;
+    }
+
+    public function getMakeAndModel()
+    {
+        return $this->vehicleMake . ' ' . $this->vehicleModel;
+    }
+}
+
+class AutomobileFactory
+{
+    public static function create($make, $model)
+    {
+        return new Automobile($make, $model);
+    }
+}
