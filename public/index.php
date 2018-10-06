bf2edd2c37e9dcfeb8066c2909fec88de2fad827
Vaishnavi <vj222@njit.edu>
2018-10-05 23:41:26 -0400

    Task : Check csv file read

--- public/index.php
+++ public/index.php
@@ -5,4 +5,18 @@
  * Date: 10/2/18
  * Time: 7:31 PM
  */
-echo 'Hello World';
+main::start( );
+
+class main
+{
+    static public function start(){
+        $file = fopen("example.csv","r");
+
+        while(! feof($file))
+        {
+            print_r(fgetcsv($file));
+        }
+
+        fclose($file);
+    }
+}
