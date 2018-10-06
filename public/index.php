14a0f53e3d242c06590d4a5e8218a4460a72cbbe
Vaishnavi <vj222@njit.edu>
2018-10-06 19:43:26 -0400

    Task:  Read csv elements to array

--- public/index.php
+++ public/index.php
@@ -1,8 +1,26 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: vaishnavi
- * Date: 10/2/18
- * Time: 7:31 PM
+
  */
-echo 'Hello World';
+main::start(  "example.csv");
+
+class main
+{
+
+
+    static public function start()
+    {
+        $file = fopen("example.csv","r");
+        while(! feof($file))
+        {
+            $record = fgetcsv($file);
+            $records[]= $record;
+
+        }
+        fclose($file);
+        print_r($record);
+    }
+
+}
+
+
