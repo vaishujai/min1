94befe2756a25a223a36bcca61cc2f9bcbead0eb
Vaishnavi <vj222@njit.edu>
2018-10-06 20:37:33 -0400

    Feature : Adding Html table(In progress)

--- public/index.php
+++ public/index.php
@@ -9,25 +9,64 @@ class main  {
         $table = html::generateTable($records);
     }
 }
-class html {
-    public static function generateTable($records) {
+class html
+{
+    public static function generateTable($records)
+    {
+       // print_r($records);
         $count = 0;
         foreach ($records as $record) {
-            if($count == 0) {
+            if ($count == 0) {
                 $array = $record->returnArray();
                 $fields = array_keys($array);
                 $values = array_values($array);
+
                 print_r($fields);
-               print_r($values);
-            } else {
+                $count = 0;
+
+                if ($count == 0) {
+                    $array = $record->returnArray();
+                    $fields = array_keys($array);
+                    echo '<table border="1">';
+                    echo "<tr><td>First</td><td>last</td><td>UCID</td><td>grade</td><td>no</td>";
+
+                    $values = array_values($array);
+                    print_r($fields);
+                    echo '<tr>';
+                    foreach ($values as $value) {
+                        echo '<td>' . $value . '</td>';
+                    }
+                    echo '</tr>';
+
+                    print_r($values);
+
+                } else {
+                    $array = $record->returnArray();
+                    $values = array_values($array);
+                    //    echo "<tr><td>";
+                    echo '<tr>';
+                    foreach ($values as $value) {
+                        echo '<td>' . $value . '</td>';
+                    }
+                    echo '</tr>';
+
+                }
+                echo '</table>';
+                $count++;
+            } // print_r($values);
+
+            else {
                 $array = $record->returnArray();
-               $values = array_values($array);
+
+                $values = array_values($array);
                 print_r($values);
             }
             $count++;
         }
     }
 }
+
+
 class csv {
     static public function getRecords($filename) {
         $file = fopen($filename,"r");
